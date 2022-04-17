35ecd7692068f92116bbd5a4749249f778a94792
Prince Dhingra <princedhingra25@gmail.com>
2022-04-17 12:42:16 -0400

    Create rightViewUsingRecusrion.cpp

---
+++ rightViewUsingRecusrion.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+#include<queue>
+#include<map>
+
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node* left;
+        Node* right;
+
+    Node(int data) {
+        this -> data = data;
+        this -> left = NULL;
+        this -> right = NULL;
+    }
+};
+
+Node* buildTree(Node* &root) {
+    cout<<"Enter data to be inserted : ";
+    int data;
+    cin>>data;
+
+    root = new Node(data);
+
+    if(data == -1)
+        return NULL;
+
+    cout<<"Enter data to be inserted on left of "<<data;
+    root -> left = buildTree(root -> left);
+    cout<<"Enter data to be inserted on right of "<<data;
+    root -> right = buildTree(root -> right);
+
+    return root;
+}
+
+void rightView(Node* temp, int level, vector<int> &result) {
+    if(temp == NULL) {
+        return;
+    }
+
+    if(result.size() == level)
+        result.push_back(temp -> data);
+
+    rightView(temp -> right, level + 1, result);
+    rightView(temp -> left, level + 1, result);
+} 
+
+int main() {
+    Node* root = NULL;
+
+    root = buildTree(root);
+
+    //2 3 5 -1 -1 -1 4 -1 -1
+    //1 2 4 -1 -1 5 -1 -1 3 6 -1 -1 7 -1 -1
+    //1 2 4 -1 -1 5 8 -1 -1 -1 3 6 -1 9 -1 -1 7 -1 10 11 -1 -1 -1
+    vector<int> result;
+    rightView(root, 0, result);
+
+    for(int i = 0; i < result.size();i++)
+        cout<<result[i]<<" ";
+
+    return 0;
+}
