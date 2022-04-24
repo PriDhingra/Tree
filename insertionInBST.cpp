3a7c9d3cd8f7bc4e7e14fd422af29a942a2716e0
Prince Dhingra <princedhingra25@gmail.com>
2022-04-24 13:35:44 -0400

    Create insertionInBST.cpp

---
+++ insertionInBST.cpp
@@ -0,0 +1,78 @@
+#include<iostream>
+#include<queue>
+
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node* left;
+        Node* right;
+
+    Node(int data) {
+        this -> data = data;
+        this -> left = NULL;
+        this -> right = NULL;
+    }
+};
+
+void levelOrderTraversal(Node* root) {
+    queue<Node*> q;
+    q.push(root);
+    q.push(NULL);
+
+    while(!q.empty()) {
+        Node* temp = q.front();
+        q.pop();
+
+        if(temp == NULL) {
+            cout<< endl;
+            if(!q.empty()) {
+                q.push(NULL);
+            }
+        } else {
+            cout<<temp -> data<<" ";
+            if(temp -> left) {
+                q.push(temp -> left);
+            }
+
+            if(temp -> right) {
+                q.push(temp -> right);
+            }
+        }
+    }
+}
+
+Node* insertInBST(Node* root, int data) {
+    if(root == NULL) {
+        Node* temp = new Node(data);
+        return temp;
+    }
+
+    if(data < root -> data) {
+        root -> left = insertInBST(root -> left, data);
+    } else {
+        root -> right = insertInBST(root -> right, data);
+    }
+
+    return root;
+}
+
+void buildBST(Node* &root) {
+    int data;
+    cin>>data;
+
+    while(data != -1) {
+        root = insertInBST(root, data);
+        cin>>data;
+    }
+}
+
+int main() {
+    Node* root = NULL;
+
+    buildBST(root);
+    levelOrderTraversal(root);
+
+    return 0;
+}
