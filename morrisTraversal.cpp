10d18ba898cbf5aab421551ebb08bbd9548ba260
Prince Dhingra <princedhingra25@gmail.com>
2022-04-23 23:59:32 -0400

    Create morrisTraversal.cpp

---
+++ morrisTraversal.cpp
@@ -0,0 +1,78 @@
+#include<iostream>
+#include<queue>
+#include<map>
+
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node* left;
+        Node* right;
+
+    Node(int data) {
+        this -> data = data;
+        this -> left = NULL;
+        this -> right = NULL;
+    }
+};
+
+Node* buildTree(Node* &root) {
+    cout<<"Enter data to be inserted : ";
+    int data;
+    cin>>data;
+
+    root = new Node(data);
+
+    if(data == -1)
+        return NULL;
+
+    cout<<"Enter data to be inserted on left of "<<data;
+    root -> left = buildTree(root -> left);
+    cout<<"Enter data to be inserted on right of "<<data;
+    root -> right = buildTree(root -> right);
+
+    return root;
+}
+
+void morrisTraversal(Node* root) {
+    Node* current = root;
+    Node* pre = NULL;
+
+    if(root == NULL)
+        return;
+
+    while(current != NULL) {
+        if(current -> left == NULL) {
+            cout<<current -> data<<" ";
+            current = current -> right; 
+        } else {
+            pre = current -> left;
+            while(pre -> right != NULL && pre -> right != current) {
+                pre = pre -> right;
+            }
+
+            if(pre -> right == NULL) {
+                pre -> right = current;
+                current = current -> left;
+            } else {
+                pre -> right = NULL;
+                cout<<current -> data<<" ";
+                current = current -> right;
+            }
+        }
+    }
+}
+
+int main() {
+    Node* root = NULL;
+
+    root = buildTree(root);
+
+    //2 3 5 -1 -1 -1 4 -1 -1
+    //1 2 4 -1 -1 5 -1 -1 3 6 -1 -1 7 -1 -1
+    //1 2 4 -1 -1 5 8 -1 -1 -1 3 6 -1 9 -1 -1 7 -1 10 11 -1 -1 -1
+    morrisTraversal(root);
+
+    return 0;
+}
