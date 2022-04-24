064c0725d215793bff86a972a4633ec409fbb255
Prince Dhingra <princedhingra25@gmail.com>
2022-04-24 14:36:19 -0400

    Create deletionInBST.cpp

---
+++ deletionInBST.cpp
@@ -0,0 +1,139 @@
+#include<iostream>
+#include<queue>
+
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node* left;
+        Node* right;
+
+    Node(int data) {
+        this -> data = data;
+        this -> left = NULL;
+        this -> right = NULL;
+    }
+};
+
+void levelOrderTraversal(Node* root) {
+    queue<Node*> q;
+    q.push(root);
+    q.push(NULL);
+
+    while(!q.empty()) {
+        Node* temp = q.front();
+        q.pop();
+
+        if(temp == NULL) {
+            cout<< endl;
+            if(!q.empty()) {
+                q.push(NULL);
+            }
+        } else {
+            cout<<temp -> data<<" ";
+            if(temp -> left) {
+                q.push(temp -> left);
+            }
+
+            if(temp -> right) {
+                q.push(temp -> right);
+            }
+        }
+    }
+}
+
+Node* insertInBST(Node* root, int data) {
+    if(root == NULL) {
+        Node* temp = new Node(data);
+        return temp;
+    }
+
+    if(data < root -> data) {
+        root -> left = insertInBST(root -> left, data);
+    } else {
+        root -> right = insertInBST(root -> right, data);
+    }
+
+    return root;
+}
+
+void buildBST(Node* &root) {
+    int data;
+    cin>>data;
+
+    while(data != -1) {
+        root = insertInBST(root, data);
+        cin>>data;
+    }
+}
+
+Node* minElementInBST(Node* root) {
+    Node* temp = root;
+
+    while(temp -> left != NULL)
+        temp = temp -> left;
+
+    return temp;
+}
+
+Node* maxElementInBST(Node* root) {
+    Node* temp = root;
+
+    while(temp -> left != NULL)
+        temp = temp -> right;
+
+    return temp;
+}
+
+Node* deleteInBST(Node* root, int data) {
+    if(root == NULL)
+        return root;
+
+    if(data < root -> data) {
+        root -> left = deleteInBST(root -> left, data);
+        return root;
+    }
+    else if(data > root -> data) { 
+        root -> right = deleteInBST(root -> right, data);
+        return root;
+    }
+    else {
+        if(root -> left == NULL && root -> right == NULL) {
+            delete(root);
+            return NULL;
+        } else if(root -> left == NULL && root -> right != NULL){
+            Node* temp = root -> right;
+            delete(root);
+            return temp;
+        } else if(root -> left != NULL && root -> right == NULL){
+            Node* temp = root -> left;
+            delete(root);
+            return temp;
+        } else {
+            int maxi = maxElementInBST(root -> left) -> data;
+            root -> data = maxi;
+            root -> left = deleteInBST(root -> left, maxi);
+            return root;
+        } 
+            
+    }
+}
+
+int main() {
+    Node* root = NULL;
+
+    buildBST(root);
+    levelOrderTraversal(root);
+
+    root = deleteInBST(root, 60);
+    levelOrderTraversal(root);
+    root = deleteInBST(root, 70);
+    levelOrderTraversal(root);
+    root = deleteInBST(root, 42);
+    levelOrderTraversal(root);
+    root = deleteInBST(root, 40);
+    levelOrderTraversal(root);
+
+    return 0;
+}
