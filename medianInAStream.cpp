8e4aa5e987a91d39ac1fb8ed1d15bb54170aa26e
Prince Dhingra <princedhingra25@gmail.com>
2022-05-02 20:45:28 -0400

    Create medianInAStream.cpp

---
+++ medianInAStream.cpp
@@ -0,0 +1,67 @@
+#include<queue>
+
+int signum(int size1, int size2) {
+	if(size1 == size2)
+		return 0;
+	else if(size1 > size2)
+		return 1;
+	else
+		return -1;
+}
+
+void callMedian(int element, priority_queue<int> &maxHeap, 
+			   priority_queue<int, vector<int>, greater<int>> &minHeap,
+			   int &median) {
+	
+	switch(signum(maxHeap.size(), minHeap.size())) {
+			
+		case 0: if(element > median) {
+					minHeap.push(element);
+					median = minHeap.top();
+				} else {
+					maxHeap.push(element);
+					median = maxHeap.top();
+				}
+				break;
+			
+		case 1: if(element > median) {
+					minHeap.push(element);
+					median = (minHeap.top() + maxHeap.top()) / 2;
+		       	} else {
+					minHeap.push(maxHeap.top());
+					maxHeap.pop();
+					maxHeap.push(element);
+					median = (minHeap.top() + maxHeap.top()) / 2;
+				}
+				break;
+			
+		case -1: if(element > median) {
+					maxHeap.push(minHeap.top());
+					minHeap.pop();
+					minHeap.push(element);
+					median = (minHeap.top() + maxHeap.top()) / 2;
+		       	} else {
+					maxHeap.push(element);
+					median = (minHeap.top() + maxHeap.top()) / 2;
+				}
+				break;
+			
+	}
+	
+}
+
+vector<int> findMedian(vector<int> &arr, int n){
+	
+	// Write your code here 
+	int median = 0;
+	priority_queue<int> maxHeap;
+	priority_queue<int, vector<int>, greater<int>> minHeap;
+	
+	vector<int> ans;
+	for(int i = 0;i < n;i++) {
+		callMedian(arr[i], maxHeap, minHeap, median);
+		ans.push_back(median);
+	}
+	
+	return ans;
+}
